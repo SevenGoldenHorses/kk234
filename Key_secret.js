c4bd29976aa27b53b4aa1fcd7eb550fb596dfe80
Federico Di Gesù <federicodigesu@MBP-di-Federico.lan>
2018-03-03 17:01:38 +0100

    primo Dep

---
+++ Key_secret.js
@@ -0,0 +1,8 @@
+module.exports = {
+  consumer_key:         'IJtbtg7R43NveCFOA6b4KRl3i',
+  consumer_secret:      'Z1dTD3THL2CoHo68jA4RW3AFNtj6jMKJAOrGlz2DEZBBx1VxK3',
+  access_token:         '390559577-El2A1Pq1Cz3NKTRrIjWW6152QLvkd6BKGAQ165Bj',
+  access_token_secret:  'ioivIhfM42EBV6vkfz7vbf7xptiDkOlO2GExc7MksuFDe',
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
+
