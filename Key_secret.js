ce796acd9d7a1fd381761ec1cbc1e6cd3eeba126
MisterX <you@example.com>
2018-04-17 19:06:40 +0200

    cambiato Token

--- Key_secret.js
+++ Key_secret.js
@@ -1,8 +1,7 @@
 module.exports = {
   consumer_key:         'IJtbtg7R43NveCFOA6b4KRl3i',
   consumer_secret:      'Z1dTD3THL2CoHo68jA4RW3AFNtj6jMKJAOrGlz2DEZBBx1VxK3',
-  access_token:         '390559577-El2A1Pq1Cz3NKTRrIjWW6152QLvkd6BKGAQ165Bj',
-  access_token_secret:  'ioivIhfM42EBV6vkfz7vbf7xptiDkOlO2GExc7MksuFDe',
+  access_token:         '390559577-naDsWPaBI523F0QHYdnRKWMQU4Dp3fvyaBewwIqm',
+  access_token_secret:  'ekRmNIdPv0kyWqrbMTOxyHqWIly4B7dQABsDwpEbEWGUz',
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
-
